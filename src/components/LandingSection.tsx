31711a19d8f1a8e65069acf832cdb04edc6d8765
AveryNewhart <newhartreeceavery@gmail.com>
2025-02-13 20:50:29 -0500

    creating landing section component

---
+++ src/components/LandingSection.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from '@mantine/core';
+
+const LandingSection = () => {
+  return (
+    <>
+      <Container fluid >
+        <p>Landing section, have a red background</p>
+        <p>photo</p>
+        <p>slogan n name</p>
+      </Container>
+    </>
+  );
+};
+
+export default LandingSection;
